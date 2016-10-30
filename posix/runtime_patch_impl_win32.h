4337ca9b92006c2fb5715f1c55d88820badd88b5
Vlad Vesa <vlad.vesa89@gmail.com>
2016-10-30 10:13:00 +0200

    implemented patching algorithm for __WIN__ platforms
    created a "wrapper" like method over VirtualProtect windows method.
    Solution found from:
    https://github.com/go-qml/qml/commit/5bad8a57a656ee2514b0672894304931f0284413
    and the page size solution found
    http://stackoverflow.com/questions/150294/how-to-programmatically-get-the-cpu-cache-page-size-in-c

--- posix/runtime_patch_impl_win32.h
+++ posix/runtime_patch_impl_win32.h
@@ -8,26 +8,69 @@
 #ifndef CPP_FREE_MOCK_POSIX_RUNTIME_PATCH_IMPL_H_
 #define CPP_FREE_MOCK_POSIX_RUNTIME_PATCH_IMPL_H_
 
-#include <cerrno>
+#include <windows.h>
 
+#define protREAD  1
+#define protWRITE 2
+#define protEXEC  4
+
+extern "C" {
+    int mprotect(void *addr, size_t len, int prot) {
+        DWORD wprot = 0;
+        if (prot & protWRITE) {
+            wprot = PAGE_READWRITE;
+        } else if (prot & protREAD) {
+            wprot = PAGE_READONLY;
+        }
+
+        if (prot & protEXEC) {
+            wprot <<= 4;
+        }
+
+        DWORD oldwprot;
+        if (!VirtualProtect(addr, len, wprot, &oldwprot)) {
+            return -1;
+        }
+
+        return 0;
+
+    }
+
+
+
+} // extern "C"
+
+#include <stdlib.h>
+#include <cerrno>
 #include <unistd.h>
 
-namespace CppFreeMock {
 
+namespace CppFreeMock {
     namespace RuntimePatcherImpl {
         // To be simple, I don't create .cpp for this static value, so you can't include this file in 2 or more cpp file.
 
         // return 0 for success, otherwire for error.
         static int UnprotectMemory(const void* const address, const size_t length) {
-            (void)address;
-            (void)length;
-            return 0;
+            SYSTEM_INFO systemInfo;
+            GetSystemInfo(&systemInfo);
+            // Here we assume that page size is power of 2.
+            void* const page = reinterpret_cast<void*>(AlignAddress(reinterpret_cast<std::size_t>(address), systemInfo.dwPageSize));
+            int ret = mprotect(page, length, protREAD | protWRITE | protEXEC);
+            if (ret != 0) {
+                int err = errno;
+                std::cerr << "Unprotect memory: " << address
+                          << " meet errno: " << err
+                          << " description: " << strerror(err) << std::endl;
+            }
+            return ret;
         }
 
         // return 0 for success, otherwire for error.
+        // return 0 for success, otherwire for error.
         static int UnprotectMemoryForOnePage(void* const address) {
-            (void) address;
-            return 0;
+            SYSTEM_INFO systemInfo;
+            GetSystemInfo(&systemInfo);
+            return UnprotectMemory(address, systemInfo.dwPageSize);
         }
     }
 
